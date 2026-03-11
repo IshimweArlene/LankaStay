3ad3f772295a82570d23a45655e79b8acc493534
IshimweArlene <ishimwearlene74@gmail.com>
2026-03-11 09:17:25 +0200

    added details

--- src/components/MostPicked.jsx
+++ src/components/MostPicked.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import img1 from '../assets/img1.jpg'
 import img2 from '../assets/img2.jpg'
 import img3 from '../assets/img3.jpg'
@@ -15,14 +16,24 @@ import room2 from '../assets/room2.jpg'
 import room3 from '../assets/room3.jpg'
 
 const MostPicked = () => {
+  const navigate = useNavigate();
+
+  const handlePriceClick = (e) => {
+    e.preventDefault();
+    navigate('/details');
+  };
+
   return (
     <div className='bg-white mx-24'>
         <h2 className='text-3xl font-bold text-blue-950 mb-6 mx-20'>Most Picked</h2>
         <div>
             <div className='grid grid-cols-3 grid-rows-2 gap-4 mx-12 mb-36'>
-                <div className='relative row-span-2'>
+                <div className='relative row-span-2 mr-8'>
                     <img src={img1} alt="Most Picked 1" className='w-[360px] h-[530px] ml-8 rounded-3xl'/>
-                    <div className='absolute top-0 rounded-tr-3xl right-3 text-white text-center text-xl w-48 h-12 bg-blue-600 p-2 rounded-lg shadow-md'>
+                    <div
+                        onClick={handlePriceClick}
+                        className='absolute top-0 rounded-tr-3xl right-3 text-white text-center text-xl w-48 h-12 bg-blue-600 p-2 rounded-lg shadow-md cursor-pointer hover:bg-blue-700 transition-colors'
+                    >
                         50$ per night
                     </div>
                     <div className='absolute bottom-14 left-14 text-slate-50 text-2xl'>
@@ -34,7 +45,10 @@ const MostPicked = () => {
                 </div>
                 <div className='relative'>
                     <img src={img2} alt="Most Picked 1" className='w-[390px] h-[250px] rounded-3xl'/>
-                    <div className='absolute top-0 rounded-tr-3xl right-3 text-white text-center text-xl w-48 h-12 bg-blue-700 p-2 rounded-lg shadow-md'>
+                    <div
+                        onClick={handlePriceClick}
+                        className='absolute top-0 rounded-tr-3xl right-3 text-white text-center text-xl w-48 h-12 bg-blue-700 p-2 rounded-lg shadow-md cursor-pointer hover:bg-blue-800 transition-colors'
+                    >
                         22$ per night
                     </div>
                     <div className='absolute bottom-14 left-14 text-slate-50 text-2xl'>
@@ -45,8 +59,11 @@ const MostPicked = () => {
                     </div>
                 </div>
                 <div className='relative'>
-                    <img src={img3} alt="Most Picked 1" className='w-[390px] h-[250px] rounded-3xl'/>  
-                    <div className='absolute top-0 rounded-tr-3xl right-3 text-white text-center text-xl w-48 h-12 bg-blue-700 p-2 rounded-lg shadow-md'>
+                    <img src={img3} alt="Most Picked 1" className='w-[390px] h-[250px] rounded-3xl'/>
+                    <div
+                        onClick={handlePriceClick}
+                        className='absolute top-0 rounded-tr-3xl right-3 text-white text-center text-xl w-48 h-12 bg-blue-700 p-2 rounded-lg shadow-md cursor-pointer hover:bg-blue-800 transition-colors'
+                    >
                         856$ per night
                     </div>
                     <div className='absolute bottom-14 left-14 text-slate-50 text-2xl'>
@@ -58,7 +75,10 @@ const MostPicked = () => {
                 </div>
                 <div className='relative'>
                     <img src={img4} alt="Most Picked 1" className='w-[390px] h-[250px] rounded-3xl'/>
-                    <div className='absolute top-0 rounded-tr-3xl right-3 text-white text-center text-xl w-48 h-12 bg-blue-700 p-2 rounded-lg shadow-md'>
+                    <div
+                        onClick={handlePriceClick}
+                        className='absolute top-0 rounded-tr-3xl right-3 text-white text-center text-xl w-48 h-12 bg-blue-700 p-2 rounded-lg shadow-md cursor-pointer hover:bg-blue-800 transition-colors'
+                    >
                         62$ per night
                     </div>
                     <div className='absolute bottom-14 left-14 text-slate-50 text-2xl'>
@@ -70,7 +90,10 @@ const MostPicked = () => {
                 </div>
                 <div className='relative'>
                     <img src={img6} alt="Most Picked 1" className='w-[390px] h-[250px] rounded-3xl'/>
-                    <div className='absolute top-0 rounded-tr-3xl right-3 text-white text-center text-xl w-48 h-12 bg-blue-700 p-2 rounded-lg shadow-md'>
+                    <div
+                        onClick={handlePriceClick}
+                        className='absolute top-0 rounded-tr-3xl right-3 text-white text-center text-xl w-48 h-12 bg-blue-700 p-2 rounded-lg shadow-md cursor-pointer hover:bg-blue-800 transition-colors'
+                    >
                         72$ per night
                     </div>
                     <div className='absolute bottom-14 left-14 text-slate-50 text-2xl'>
@@ -79,7 +102,7 @@ const MostPicked = () => {
                     <div className='absolute bottom-6 left-14 text-slate-100 text-xl'>
                         Kandy, Sri Lanka
                     </div>
-                </div>           
+                </div>
             </div>
 
             <div className='grid grid-cols-4 gap-4 mx-12 mb-24 '>
@@ -124,7 +147,7 @@ const MostPicked = () => {
                         <p className='font-semibold text-black text-2xl'>Wooden Pit</p>
                         <p className='text-xl text-gray-700'> Ambalagonde, Sri Lanka</p>
                     </div>
-                </div> 
+                </div>
 
                 <div className='relative'>
                     <div>
@@ -134,7 +157,7 @@ const MostPicked = () => {
                         <p className='font-semibold text-black text-2xl'>Boutique</p>
                         <p className='text-xl text-gray-700'> Kandy, Sri Lanka</p>
                     </div>
-                </div> 
+                </div>
                 <div className='relative'>
                     <div>
                         <img src={room1} className='w-[390px] h-[250px] rounded-3xl mx-3'/>
@@ -143,7 +166,7 @@ const MostPicked = () => {
                         <p className='font-semibold text-black text-2xl'>Modern</p>
                         <p className='text-xl text-gray-700'> Nuwereliya, Sri Lanka</p>
                     </div>
-                </div> 
+                </div>
                 <div className='relative'>
                     <div>
                         <img src={room2} className='w-[390px] h-[250px] rounded-3xl mx-3'/>
@@ -152,7 +175,7 @@ const MostPicked = () => {
                         <p className='font-semibold text-black text-2xl'>Silver Rain</p>
                         <p className='text-xl text-gray-700'> Dehiwala, Sri Lanka</p>
                     </div>
-                </div> 
+                </div>
                 <div className='relative'>
                     <div>
                         <img src={img8} className='w-[390px] h-[250px] rounded-3xl mx-3'/>
@@ -164,8 +187,8 @@ const MostPicked = () => {
                         <p className='font-semibold text-black text-2xl'>CashVille</p>
                         <p className='text-xl text-gray-700'> Ampara, Sri Lanka</p>
                     </div>
-                </div> 
-                              
+                </div>
+
             </div>
         </div>
     </div>
